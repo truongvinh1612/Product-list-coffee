d029981a487ebfe37f19b92cc1963f4907a83d5a
truongvinh1612 <truongvinh1612@gmail.com>
2024-03-09 15:16:03 +0700

    Quản lý sản phẩm

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js hoặc một trang/component khác
+import React from 'react';
+import ProductList from './ProductList';
+
+function App() {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LinkAPI.js
@@ -0,0 +1,2 @@
+// export const API =  'http://192.168.0.108:8886/api/products'
+export const LinkAPI =  'http://localhost:8089/api/products'
